21046721146e5a18314e2ad078cb3dfcd9450567
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2023-09-05 10:17:09 +0100

    Update ContentView.swift
    Boilerplate code that will be amended later to suit this particular use case.

--- ContentView.swift
+++ ContentView.swift
@@ -1,1 +1,25 @@
+//
+//  ContentView.swift
+//  WatchKit App For IP2Location Watch App
+//
+//  Created by Babatunde Onabajo on 03/09/2023.
+// This is boilerplate code that will be amended later. 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to the Apple Watch app for IP2Location.io")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
 
